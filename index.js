f420328ddc2818c18b89158cc44def577f9767d7
Keith Capers <K.Capers94@gmail.com>
2024-01-18 17:38:40 -0500

    complete lab

--- index.js
+++ index.js
@@ -8,6 +8,56 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+function createEmployeeRecord (employeeArray) {
+return {
+    firstName: employeeArray[0],
+    familyName: employeeArray[1],
+    title:employeeArray[2],
+    payPerHour:employeeArray[3],
+    timeInEvents:[],
+    timeOutEvents:[],
+}
+
+}
+
+function createEmployeeRecords (arrayOfArrays) {
+    let arrayOfObjects = [];
+    arrayOfArrays.forEach((element) => arrayOfObjects.push(createEmployeeRecord(element)));
+    return arrayOfObjects
+}
+
+function createTimeInEvent (dateStamp) {
+    let timeInEventsObject = {
+        type: 'TimeIn',
+        hour: parseInt(dateStamp.split(" ")[1]),
+        date: dateStamp.split(" ")[0],
+    }
+    this['timeInEvents'].push(timeInEventsObject)
+    return this
+}
+
+function createTimeOutEvent (dateStamp) {
+    let timeOutEventsObject = {
+        type: 'TimeOut',
+        hour: parseInt(dateStamp.split(" ")[1]),
+        date: dateStamp.split(" ")[0],
+    }
+    this['timeOutEvents'].push(timeOutEventsObject)
+    return this
+}
+
+function hoursWorkedOnDate (dateOfTheForm) {
+    let timeInObject = this['timeInEvents'].find((element) => dateOfTheForm === element ['date'])
+    let timeOutObject = this['timeOutEvents'].find((element) => dateOfTheForm === element ['date'])
+    return (timeOutObject['hour'] - timeInObject['hour']) / 100
+}
+
+function wagesEarnedOnDate (dateOfTheForm) {
+    console.log(this)
+    let wagesEarnedObject = this['payPerHour']
+    return wagesEarnedObject * hoursWorkedOnDate.call(this,dateOfTheForm)
+}
+
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
@@ -19,5 +69,15 @@ const allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function findEmployeeByFirstName (collection, firstNameString) {
+    const findsFirstName = collection.find((element) => firstNameString === element['firstName'])
+    return findsFirstName
+}
+
+function calculatePayroll (employeeRecords) {
+    let payrollAllEmployees = employeeRecords.reduce((acc, employeeRecords) => acc + allWagesFor.call(employeeRecords),0)
+    return payrollAllEmployees
 }
 
